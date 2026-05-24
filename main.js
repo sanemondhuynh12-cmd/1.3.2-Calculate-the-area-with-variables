42c8261ab7a4f8c0a42bd1ba28cf5b4989b6862e
sanemondhuynh12-cmd <sanemond.huynh12@gmail.com>
2026-05-24 05:33:52 +0000

    Area assignment

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-var height = prompt("Enter the height", 150);
-var width = prompt("Enter the width", 230);
+var height = prompt(150);
+var width = prompt(230);
     
 // Write your code below
-
-
-
+let area = width*height;
 
 // Print out result in the console log statement below 
-console.log();
+console.log(area);
